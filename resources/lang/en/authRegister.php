41761c88b16a18c16ca5accb21592ed33e398778
MostafaElshehawey201 <elshehaweymostafa@gmail.com>
2025-12-26 15:46:34 +0200

    Add localization support (Arabic & English)

---
+++ resources/lang/en/authRegister.php
@@ -0,0 +1,11 @@
+<?php
+    return[
+        "name" => "UserName",
+        "email" => "Email",
+        "phone" => "Phone Number",
+        "password" => "Password",
+        "Media_reporter" => "Media Reporter",
+        "visitor" => "Visitor",
+        "signup" => "Sign Up",
+    ]
+?>
